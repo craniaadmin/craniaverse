26f1412b7b7f7e236ef3cc159b007ccfbf5becaa
Hana Karim <anah.mirak@gmail.com>
2026-07-30 20:04:56 -0400

    Update

--- src/__clharness.jsx
+++ src/__clharness.jsx
@@ -1,5 +1,22 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import ClassLists from './pages/ClassLists.jsx'
 import './index.css'
+
+class Boundary extends React.Component {
+  constructor(p) { super(p); this.state = { err: null } }
+  static getDerivedStateFromError(err) { return { err } }
+  render() {
+    if (this.state.err) {
+      return <pre id="harness-error" style={{ whiteSpace: 'pre-wrap', color: '#a12626' }}>
+        {String((this.state.err && this.state.err.stack) || this.state.err)}
+      </pre>
+    }
+    return this.props.children
+  }
+}
+
 createRoot(document.getElementById('root')).render(
-  <div style={{ padding: 20, background: '#F4F7F8', minHeight: '100vh' }}><ClassLists /></div>)
+  <div style={{ padding: 20, background: '#F4F7F8', minHeight: '100vh' }}>
+    <Boundary><ClassLists /></Boundary>
+  </div>)
